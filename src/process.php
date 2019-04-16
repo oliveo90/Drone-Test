901be6b12d330a421bfa392bbd9eea042c382853
oliveo90 <38668148+oliveo90@users.noreply.github.com>
2019-04-16 02:19:46 -0600

    Update process.php

--- src/process.php
+++ src/process.php
@@ -39,3 +39,38 @@
                         return $trip_loc->get_location();
                 }
         }
+
+        # Sanitize POST array to remove unwanted characters for security
+        if( $_SERVER['REQUEST_METHOD']=='POST' && isset( $_POST['submit'] ) ){
+
+            $truck_name = filter_input( INPUT_POST, 'truck_name', FILTER_SANITIZE_STRING );
+            $max_weight = filter_input( INPUT_POST, 'max_weight', FILTER_SANITIZE_STRING );
+
+            $location_1 = filter_input( INPUT_POST, 'location_1', FILTER_SANITIZE_STRING );
+            $location_2 = filter_input( INPUT_POST, 'location_2', FILTER_SANITIZE_STRING );
+            $location_3 = filter_input( INPUT_POST, 'location_3', FILTER_SANITIZE_STRING );
+
+            $package_1 = filter_input( INPUT_POST, 'package_1', FILTER_SANITIZE_STRING );
+            $package_2 = filter_input( INPUT_POST, 'package_2', FILTER_SANITIZE_STRING );
+            $package_3 = filter_input( INPUT_POST, 'package_3', FILTER_SANITIZE_STRING );
+
+                # click reset button to hide output div block
+                $style = "";
+                if( isset($_POST['reset']) ){
+                        $style = "style='display:none;'";
+                }
+
+                # set locations
+                $drone=new Drone( $drone_name, $max_weight );
+
+                $loc_1=new Location( $location_1, $package_1 );
+                $loc_2=new Location( $location_2, $package_2 );
+                $loc_3=new Location( $location_3, $package_3 );
+
+                # init Trip #1
+                $trip_1 = new Trip;
+
+                # init Trip #2
+                $trip_2 = new Trip;
+        }
+?>
