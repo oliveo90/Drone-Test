0cf7f902f5800ebded33556b7a8e3c004c9bb8dd
oliveo90 <38668148+oliveo90@users.noreply.github.com>
2019-04-16 02:15:42 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,80 +1,78 @@
+<?php
+require("./src/process.php");
+?>
 <!DOCTYPE HTML>
 <html lang="en">
-        <head>
-            <title>Drone Delivery Test App</title>
-        </head>
-        <body class="container">
- 		<h1>Drone Delivery Form</h1>
+     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="Drone Delivery Test App">
+        <meta name="author" content="Olive Ogbo">
+        <title>Drone Delivery Test App</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+     </head>
+     <body class="container">
+             
+        <h1>Drone Delivery Form</h1>
 
-    		<!-- Input form HTML -->
-            <form method="post" action="">
-    			<h3>Drone</h3>
-                <input type="text" name="drone_name" placeholder="Drone Name" value="" required>
-                <input type="number" name="max_weight" placeholder="Max weight in lbs" value="" required>
+        <!-- Input form HTML -->
+        <form method="post" action="">
+                
+        <h3>Drone</h3>
 
+           <div class="form-group row">
+              <div class="col-sm-4">
+                <input type="text" class="form-control" name="drone_name" placeholder="Drone Name" value="" required>
+                <input type="number" class="form-control" name="max_weight" placeholder="Max weight in lbs" value="" required>
+              </div>
+           </div>
                 <h3>Delivery Locations</h3>
 
+           <div class="form-group row">
+              <div class="col-sm-4">
                 <input type="text" name="location_1" placeholder="Location 1" value="" required>
                 <input type="number" name="package_1" placeholder="Package weight in lbs" value="" required>
+              </div>
+           </div>
+                
+           <div class="form-group row">
+              <div class="col-sm-4">          
+                <input type="text" class="form-control" name="location_2" placeholder="Location 2" value="">
+                <input type="number" class="form-control" name="package_2" placeholder="Package weight in lbs" value="" required>
+              </div>
+           </div>
+                      
+           <div class="form-group row">
+              <div class="col-sm-4">
+                <input type="text" class="form-control" name="location_3" placeholder="Location 3" value="" required>
+                <input type="number" class="form-control" name="package_3" placeholder="Package weight in lbs" value="" required>
+              </div>
+           </div>
+                      
+   <div class="form-group row">
+      <div class="col-sm-2">
+                <input type="submit" class="btn btn-primary" name="submit" value="Get Trips">
+        </form>
+                   
+        <form>
+                <input type="submit" class="btn btn-secondary" name="reset" value="Reset">
+        </form>
+           
+      </div>
+   </div>
 
-                <input type="text" name="location_2" placeholder="Location 2" value="">
-                <input type="number" name="package_2" placeholder="Package weight in lbs" value="" required>
+        
+        # output div block
+        <div <?php echo $style;?>>
+            <h2>Most Efficient Delivery Routes</h2>
 
-                <input type="text" name="location_3" placeholder="Location 3" value="" required>
-                <input type="number" name="package_3" placeholder="Package weight in lbs" value="" required>
+                <!--Output Trip #1 -->
+                <h4>Trip #1</h4> <br />
+                <?php echo $trip_1->setLocations($loc_2) . ", " . $trip_1->setLocations($loc_3);?>
 
-
-                <input type="submit" name="submit" value="Get Trips">
-            </form>
-    		<form>
-                <input type="submit" name="reset" value="Reset">
-            </form>
-            
-                <?php
-                        # Sanitize POST array to remove unwanted characters for security
-                        if( $_SERVER['REQUEST_METHOD']=='POST' && isset( $_POST['submit'] ) ){
-
-                            $truck_name = filter_input( INPUT_POST, 'truck_name', FILTER_SANITIZE_STRING );
-                            $max_weight = filter_input( INPUT_POST, 'max_weight', FILTER_SANITIZE_STRING );
-
-                            $location_1 = filter_input( INPUT_POST, 'location_1', FILTER_SANITIZE_STRING );
-                            $location_2 = filter_input( INPUT_POST, 'location_2', FILTER_SANITIZE_STRING );
-                            $location_3 = filter_input( INPUT_POST, 'location_3', FILTER_SANITIZE_STRING );
-
-                            $package_1 = filter_input( INPUT_POST, 'package_1', FILTER_SANITIZE_STRING );
-                            $package_2 = filter_input( INPUT_POST, 'package_2', FILTER_SANITIZE_STRING );
-                            $package_3 = filter_input( INPUT_POST, 'package_3', FILTER_SANITIZE_STRING );
-                    	
-                        # click reset button to hide output div block
-                    	$style = "";
-                        if( isset($_POST['reset']) ){
-                                $style = "style='display:none;'";
-                        }
-
-                        # output div block
-                        echo "<div " . $style . ">";
-                                echo '<h2>Most Efficient Delivery Routes</h2>';
-
-                                # set locations
-                                $drone=new Drone( $drone_name, $max_weight );
-
-                                $loc_1=new Location( $location_1, $package_1 );
-                                $loc_2=new Location( $location_2, $package_2 );
-                                $loc_3=new Location( $location_3, $package_3 );
-
-                                # Output Trip #1
-                                echo '<h4>Trip #1</h4>';
-
-                                $trip_1 = new Trip;
-                                echo $trip_1->setLocations($loc_2) . ", " . $trip_1->setLocations($loc_3);
-
-                                # Output Trip #2
-                                echo '<br /> <h4>Trip #2</h4>';
-
-                                $trip_2 = new Trip;
-                                echo $trip_2->setLocations($loc_1);;
-                        echo "</div>";
-                }
-        ?>
-        </body>
+                # Output Trip #2
+                <h4>Trip #2</h4>
+                <?php echo $trip_2->setLocations($loc_1);?>
+        </div>
+             
+    </body>
 </html>
