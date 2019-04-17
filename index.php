d10fc13734d69c2ebc3c1b2946403878b77c15fd
oliveo90 <38668148+oliveo90@users.noreply.github.com>
2019-04-17 13:55:41 -0600

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ require("./src/process.php");
      <body class="container">
              
         <h1>Drone Delivery Form</h1>
+       <div class="row">
 
         <!-- Input form HTML -->
         <form method="post" action="">
@@ -20,58 +21,70 @@ require("./src/process.php");
         <h3>Drone</h3>
 
            <div class="form-group row">
-              <div class="col-sm-4">
+              <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
                 <input type="text" class="form-control" name="drone_name" placeholder="Drone Name" value="" required>
+              </div>
+              <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
                 <input type="number" class="form-control" name="max_weight" placeholder="Max weight in lbs" value="" required>
               </div>
            </div>
                 <h3>Delivery Locations</h3>
 
            <div class="form-group row">
-              <div class="col-sm-4">
-                <input type="text" name="location_1" placeholder="Location 1" value="" required>
-                <input type="number" name="package_1" placeholder="Package weight in lbs" value="" required>
+              <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                <input type="text" class="form-control" name="location_1" placeholder="Location 1" value="" required>
+              </div>
+              <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                <input type="number" class="form-control" name="package_1" placeholder="Package weight in lbs" value="" required>
               </div>
            </div>
                 
            <div class="form-group row">
-              <div class="col-sm-4">          
+              <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">          
                 <input type="text" class="form-control" name="location_2" placeholder="Location 2" value="">
+              </div>
+              <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
                 <input type="number" class="form-control" name="package_2" placeholder="Package weight in lbs" value="" required>
               </div>
            </div>
                       
            <div class="form-group row">
-              <div class="col-sm-4">
+              <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
                 <input type="text" class="form-control" name="location_3" placeholder="Location 3" value="" required>
+              </div>
+              <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
                 <input type="number" class="form-control" name="package_3" placeholder="Package weight in lbs" value="" required>
               </div>
            </div>
                       
-   <div class="form-group row">
-      <div class="col-sm-2">
-                <input type="submit" class="btn btn-primary" name="submit" value="Get Trips">
+            <div class="form-group row">
+                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                    <input type="submit" class="btn btn-primary" name="submit" value="Get Trips">
+                </div>
+            </div>
         </form>
-                   
-        <form>
-                <input type="submit" class="btn btn-secondary" name="reset" value="Reset">
-        </form>
-           
-      </div>
+      
    </div>
 
         
-        # output div block
-        <div <?php echo $style;?>>
-            <h2>Most Efficient Delivery Routes</h2>
+        <!-- output div block -->
+        <div class="row" <?php echo $style; ?>>
+            <h4 style="margin-top: 20px;">Most Efficient Delivery Routes</h4>
 
-                <!--Output Trip #1 -->
-                <h4>Trip #1</h4> <br />
-                <?php echo $trip_1->setLocations($loc_2) . ", " . $trip_1->setLocations($loc_3);?>
-
-                # Output Trip #2
+<h5><?php echo $drone->get_name(); ?></h5>
+                <!-- Output Trip #1 -->
+                <h4>Trip #1</h4>
+                <p><?php echo $trip_1->setLocations($loc_2) . ", " . $trip_1->setLocations($loc_3);?> </p>
+                
+                <!-- Output Trip #2 -->
                 <h4>Trip #2</h4>
-                <?php echo $trip_2->setLocations($loc_1);?>
+                <p><?php echo $trip_2->setLocations($loc_1);?> </p>
+                
+            <form>
+                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                    <input type="submit" class="btn btn-secondary" name="reset" value="Reset">
+                </div>
+            </form>
         </div>
              
     </body>
